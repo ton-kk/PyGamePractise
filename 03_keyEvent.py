f6f4079928337daccb06bcae4d44175275b1fe19
ton-kk <scanf.ton@gmail.com>
2020-02-02 23:41:39 +0900

    add 03 and description

--- 03_keyEvent.py
+++ 03_keyEvent.py
@@ -1,8 +1,5 @@
-### 1.skeleton  
-make window with black background.  
-And frame rate = 20.  
+from random import random
 
-```python
 import pygame
 from pygame.locals import *
 import sys
@@ -18,30 +15,6 @@ screen = pygame.display.set_mode((wWidth, wHeight))
 pygame.display.set_caption(wStrings)
 FPSClock = pygame.time.Clock()
 
-  def main():
-
-    while True:
-        screen.fill((0, 0, 0))
-        pygame.display.update()
-        
-        # WindowClose
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-                
-       FPSClock.tick(wFPS)
-
-if __name__ == "__main__":
-    main()
-```
-  
-### 2.write words  
-Draw text in the center of the window.  
-
-```python
-# after init ...
-
 # drawWords
 dStrings = "Hello PyGame."
 font = pygame.font.SysFont(None, 60)
@@ -49,22 +22,13 @@ Word = font.render(dStrings, True, (0, 80, 150))
 Word_rect = Word.get_rect()
 Word_rect.center = (wWidth/2, wHeight/2)
 
-# in main func
+
+def main():
     while True:
         screen.fill((0, 0, 0))
         screen.blit(Word, Word_rect)
-        pygame.display.update()
-```
 
-![2_done](https://user-images.githubusercontent.com/58809086/72351111-1097fa80-3723-11ea-9889-ab7b22684dff.png)
-
-
-### 2.get key event
-it's very simple key event.  
-if press space key, the text move (0~10, 0~10)  
-
-```python
-# in loop(while True:) section
+        # WindowClose
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -73,4 +37,8 @@ if press space key, the text move (0~10, 0~10)
                 if event.key == K_SPACE:
                     Word_rect.move_ip(10 * random(), 10 * random())
         pygame.display.update()
-```
+    FPSClock.tick(wFPS)
+
+
+if __name__ == "__main__":
+    main()
